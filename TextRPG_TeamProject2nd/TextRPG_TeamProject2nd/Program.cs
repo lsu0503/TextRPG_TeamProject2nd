211b0c70ad2fa17748ebeb395ea045c40336d940
이선욱(Unity_6기) <lsu0503@gmail.com>
2024-09-26 16:39:23 +0900

    Create Project
    Create Project

---
+++ TextRPG_TeamProject2nd/TextRPG_TeamProject2nd/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TextRPG_TeamProject2nd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
