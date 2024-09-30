d9859d0d839d3562be3f34de47ccfcd5c18e2b19
이선욱(Unity_6기) <lsu0503@gmail.com>
2024-09-30 20:55:31 +0900

    [Update] Update UIManager.cs
    Update UIManager.cs

--- TextRPG_TeamProject2nd/TextRPG_TeamProject2nd/Manager/UIManager.cs
+++ TextRPG_TeamProject2nd/TextRPG_TeamProject2nd/Manager/UIManager.cs
@@ -4,11 +4,13 @@ using System.Text;
 using System.Xml;
 using TextRPG_TeamProject2nd.Manager;
 using TextRPG_TeamProject2nd.Object;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace TeamProjectBin
 {
     internal class UIManager
     {
+        // 상단 화면 타이틀을 그리는 함수.
         static void DisplayTitle(string _titleText)
         {
             Console.SetCursorPosition(0, 0);
@@ -21,6 +23,7 @@ namespace TeamProjectBin
             Console.WriteLine("====================================================================================================");
         }
 
+        // 우측 플레이어 정보를 그리는 함수.
         /*
         static void DisplayPlayerInfoRight(Player _player)
         {
@@ -49,6 +52,7 @@ namespace TeamProjectBin
         }
         */
 
+        // 타이틀 화면 관련 UI 함수
         public void DisplayStartMenu()
         {
             Console.WriteLine("[1] 새로 시작");
@@ -56,11 +60,13 @@ namespace TeamProjectBin
             Console.WriteLine("[0] 게임 종료");
         }
 
+        // 세이브 슬롯 확인 함수.
         public void DisplaySaveSlot()
         {
             // 세이브 시스템 제작 이후 작성하자.
         }
 
+        // 마을 화면 관련 UI 함수
         public void DisplayVillageMenu()
         {
             Console.WriteLine("[1] 상  점: 래비츠 인더스트리");
@@ -70,6 +76,7 @@ namespace TeamProjectBin
             Console.WriteLine("[0] 나가기: 양 한마리 여관");
         }
 
+        // 상점 관련 UI 함수.
         public void DisplayShopEntrance()
         {
             Console.WriteLine("[1] 구매하기");
@@ -77,10 +84,76 @@ namespace TeamProjectBin
             Console.WriteLine("[0] 나 가 기");
         }
 
-        public void DisplayShopMerchandise(int _index)
+        // 상점 - 구매하기
+        public void DisplayShopBuyList(int _page)
         {
-            
+            for (int i = 0 * _page; i < 9; i++)
+            {
+                if ((_page * 9) + i >= GameManager.Instance().storeList.Count)
+                    break;
+
+                Console.Write($"[{i + 1}] {GameManager.Instance().storeList[(_page * 9) + i].name} <{GameManager.Instance().storeList[(_page * 9) + i].value}#>");
+                Console.CursorLeft = 50;
+                Console.WriteLine($" | {GameManager.Instance().storeList[(_page * 9) + i].desc}");
+            }
+
+
+            if (_page > 0)
+                Console.Write($"[-] 이전         ");
+            else
+                Console.Write($"                 ");
+
+            Console.Write($"[ {_page} ]");
+
+            if ((_page + 1) * 9 < GameManager.Instance().storeList.Count)
+                Console.WriteLine($"         다음 [+]");
+            else
+                Console.WriteLine();
+
+            Console.WriteLine("[0] 나가기");
         }
+
+        // 구매 완료 시 표시.
+        public void DisplayShoBuyText(Item _itemBought)
+        {
+            Console.WriteLine($"{_itemBought.name}을 {_itemBought.value}에 구매하셨습니다.");
+        }
+
+        // 상점 - 판매하기
+        public void DisplayShopSellScreen(int _page)
+        {
+            for (int i = 0 * _page; i < 9; i++)
+            {
+                if ((_page * 9) + i >= GameManager.Instance().storageList.Count)
+                    break;
+
+                Console.WriteLine($"[{i + 1}] {GameManager.Instance().storageList[(_page * 9) + i].name}"
+                                + $"<{(int)(GameManager.Instance().storageList[(_page * 9) + i].value * 0.85f)}#>"
+                                + $" | {GameManager.Instance().storageList[(_page * 9) + i].desc}");
+            }
+
+            if (_page > 0)
+                Console.Write($"[-] 이전         ");
+            else
+                Console.Write($"                 ");
+
+            Console.Write($"[ {_page} ]");
+
+            if ((_page + 1) * 9 < GameManager.Instance().storeList.Count)
+                Console.WriteLine($"         다음 [+]");
+            else
+                Console.WriteLine();
+
+            Console.WriteLine("[0] 나가기");
+        }
+
+        // 판매 완료 시 표시.
+        public void DisplayShoSellText(Item _itemBought)
+        {
+            Console.WriteLine($"{_itemBought.name}을 {(int)(_itemBought.value * 0.85f)}에 판매하셨습니다.");
+        }
+
+
 
         public void PositionCursorToInput()
         {
@@ -98,5 +171,3 @@ namespace TeamProjectBin
         }
     }
 }
-
-
