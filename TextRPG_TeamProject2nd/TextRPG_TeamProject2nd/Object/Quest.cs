33274ab4731120141ac54d52715056878aca94f5
playbuild <148020238+playbuild@users.noreply.github.com>
2024-10-02 11:45:54 +0900

    add QuestManager

---
+++ TextRPG_TeamProject2nd/TextRPG_TeamProject2nd/Object/Quest.cs
@@ -0,0 +1,53 @@
+using TextRPG_TeamProject2nd.Manager;
+using TextRPG_TeamProject2nd.Utils;
+
+namespace TextRPG_TeamProject2nd.Quest
+{
+    internal class Quest: IClone<Quest>
+    {
+        public Quest clone()
+        {
+            Quest ret = new Quest();
+
+            ret.questId                        = questId;
+            ret.questName                      = questName;
+            ret.questDesc                      = questDesc;
+            ret.questTargetId                  = questTargetId;
+            ret.questTargetAmount              = questTargetAmount;
+            ret.questProgressAmount            = questProgressAmount;
+            ret.rewardGold                     = rewardGold;
+            ret.rewardItemId                   = rewardItemId;
+            return ret;
+        }
+
+        public void ProgressQuest(Monster defeatedMonster)
+        {
+            MobInfo mobInfo = defeatedMonster.GetMobInfo();
+
+            if(questTargetId == mobInfo.id)
+            {
+                questProgressAmount += 1;
+                if (questProgressAmount >= questTargetAmount)
+                {
+                    questProgressAmount = questTargetAmount;
+                }
+            }
+        }
+        public bool CheckProgress()
+        {
+            if(questProgressAmount >= questTargetAmount) { return true; }
+            else {  return false; }
+        }
+
+        public int questId                { get; set; }
+        public string questName           { get; set; }
+        public string qeustDesc           { get; set; } //퀘스트 내용 설명
+        public int questTargetId          { get; set; } //목표 몬스터
+        public int questTargetAmount      { get; set; } //퀘스트 목표치
+        public int questProgressAmount    { get; set; } //현재 퀘스트 달성치
+        public int rewardGold             { get; set; } //보상 골드
+        public int rewardItemId           { get; set; } //보상 아이템
+
+    }
+
+}
